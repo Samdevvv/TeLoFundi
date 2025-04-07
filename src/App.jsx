e07c248aba720e308676d96e9a02db387a0f2e58
Lonxs69 <juniiordeleon5@gmail.com>
2025-04-06 19:17:16 -0600

    login mejorado(faltan pila de vaina aun)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./componentes/login";
+import Registro from "./componentes/registro";
 import HomePage from "./componentes/homepage";
 import "./estilos/login.css";
 import "./estilos/homepage.css";
 
 function App() {
+  const [menu, setMenu] = useState("login");
+
   return (
     <div>
-     
-     {/*<HomePage/> */}
-     <Login/> 
+
+      {menu == "login" ?
+        <Login
+          setMenu={setMenu}
+        />
+        :
+        <Registro
+          setMenu={setMenu}
+        />}
     </div>
   );
 }
